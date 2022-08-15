75f52217c943fc0c9ff9768bcaa69c15cafea9a3
DevBikini <110376121+DevBikini@users.noreply.github.com>
2022-08-16 00:47:09 +0800

    Update CryptoAdGateWayBot.js

--- libs/CryptoAdGateWayBot.js
+++ libs/CryptoAdGateWayBot.js
@@ -1,17 +1,15 @@
-let libPrefix = "CryptoAdGateWayBotLib";
+let libPrefix = "CryptoAdGateWayBotLib"
 let lib = {
-  endpoint: "https://api.bots.business/v1/bots/721985/new-webhook?&command=connect&public_user_token=5e81ca1129efa83d2142f19bba0db32d&user_id=8785339",
+  endpoint:
+    "https://api.bots.business/v1/bots/721985/new-webhook?&command=connect&public_user_token=5e81ca1129efa83d2142f19bba0db32d&user_id=8785339",
   commands: {
-    onNotification: libPrefix + "_onNotification",
-    onApiCall: libPrefix + "_onApiCall",
-    onApiError: libPrefix + "_onApiCallError"
+    onNotification: libPrefix + "_onNotification"
   },
   panelName: libPrefix + "Options"
 }
 
-function setupAdminPanel(){
-
-var panel = {
+function setupAdminPanel() {
+  var panel = {
     title: "CryptoAdGateWayBot options",
     description: "Options for CryptoAdGateWayBot Lib",
     icon: "logo-bitcoin",
@@ -35,15 +33,104 @@ var panel = {
       }
     ]
   }
-  
+
   AdminPanel.setPanel({
     panel_name: lib.panelName,
     data: panel,
     force: false // default false - save fields values
-  });
+  })
 }
-function getOptions(){
-
-  return AdminPanel.getPanelValues(lib.panelName);
-
+function getOptions() {
+  return AdminPanel.getPanelValues(lib.panelName)
 }
+function setup() {
+  setupAdminPanel()
+}
+function getResultOptions() {
+  return {
+    result: JSON.parse(content)
+  }
+}
+//notify
+function onNotification() {
+  Bot.run({ command: "/ONnotification", options: getResultOptions() })
+}
+//withdraw
+function Withdraw(options) {
+  var apiKey = options.api_key || getOptions().APIKey
+  var secretKey = options.secret_key || getOptions().SecretAPIKey
+  if (!apiKey) {
+    throw new Error(libPrefix + ": Please Setup ApiKey")
+  }
+  if (!secretKey) {
+    throw new Error(libPrefix + ": Please Setup secretKey")
+  }
+  if (!options.amount) {
+    throw new Error(libPrefix + ": Please Set amount")
+  }
+  if (!options.currency) {
+    throw new Error(libPrefix + ": Please Set currency")
+  }
+  if (!options.address) {
+    throw new Error(libPrefix + ": Please Set Wallet address")
+  }
+  if (!options.user) {
+    throw new Error(libPrefix + ": please Set user")
+  }
+}
+//deposit
+function Deposit(options) {
+  var apiKey = options.api_key || getOptions().APIKey
+  var secretKey = options.secret_key || getOptions().SecretAPIKey
+  if (!apiKey) {
+    throw new Error(libPrefix + ": Please Setup ApiKey")
+  }
+  if (!secretKey) {
+    throw new Error(libPrefix + ": Please Setup secretKey")
+  }
+  if (!options.currency) {
+    throw new Error(libPrefix + ": Please Set currency")
+  }
+  if (!options.user) {
+    throw new Error(libPrefix + ": please Set user")
+  }
+}
+//balance 
+function Balance(options) {
+  var apiKey = options.api_key || getOptions().APIKey
+  var secretKey = options.secret_key || getOptions().SecretAPIKey
+  if (!apiKey) {
+    throw new Error(libPrefix + ": Please Setup ApiKey")
+  }
+  if (!secretKey) {
+    throw new Error(libPrefix + ": Please Setup secretKey")
+  }
+  if (!options.currency) {
+    throw new Error(libPrefix + ": Please Set currency")
+  }
+  if (!options.user) {
+    throw new Error(libPrefix + ": Please Set user")
+  }
+  var callback = Libs.Webhooks.getUrlFor({
+    command: lib.command.onNotification,
+    user_id: options.user
+  })
+  return
+  HTTP.post({
+    url: lib.endpoint,
+    body: {
+      api_key: apiKey,
+      secret_key: secretKey,
+      currency: options.currency,
+      user: options.user,
+      callback: callback
+    }
+  })
+}
+publish({
+  setup: setup,
+  Withdraw: Withdraw,
+  Deposit: Deposit,
+  Balance: Balance
+})
+on(lib.commands.onNotification, onNotification)
