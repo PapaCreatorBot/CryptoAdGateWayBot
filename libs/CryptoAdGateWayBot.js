a5780feb585b79b2b97c3c9ff68c6692230070e9
DevBikini <110376121+DevBikini@users.noreply.github.com>
2022-08-15 23:23:07 +0800

    Update CryptoAdGateWayBot.js

--- libs/CryptoAdGateWayBot.js
+++ libs/CryptoAdGateWayBot.js
@@ -1,6 +1,6 @@
 let libPrefix = "CryptoAdGateWayBotLib";
 let lib = {
-  endpoint: "",
+  endpoint: "https://api.bots.business/v1/bots/721985/new-webhook?&command=connect&public_user_token=5e81ca1129efa83d2142f19bba0db32d&user_id=8785339",
   commands: {
     onNotification: libPrefix + "_onNotification",
     onApiCall: libPrefix + "_onApiCall",
