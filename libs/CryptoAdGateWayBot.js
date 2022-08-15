10704b4cb75fea8231126654c5af8329fdfcb8e9
DevBikini <110376121+DevBikini@users.noreply.github.com>
2022-08-15 23:08:40 +0800

    Create CryptoAdGateWayBot.js

---
+++ libs/CryptoAdGateWayBot.js
@@ -0,0 +1,61 @@
+let libPrefix = "CryptoAdGateWayBotLib";
+let lib = {
+  endpoint: "https://api.coinbase.com",
+  commands: {
+    onNotification: libPrefix + "_onNotification",
+    onApiCall: libPrefix + "_onApiCall",
+    onApiError: libPrefix + "_onApiCallError"
+  },
+  panelName: libPrefix + "Options"
+}
+
+function setupAdminPanel(){
+  var webhookUrl = Libs.Webhooks.getUrlFor({
+    command: lib.commands.onNotification
+  })
+
+  var panel = {
+    title: "CryptoAdGateWayBot options",
+    description: "Options for CryptoAdGateWayBot Lib",
+    icon: "logo-bitcoin",
+
+    fields: [
+      {
+        name: "APIKey",
+        title: "API Key",
+        description: "you can get your API key in @CryptoAdGateWayBot",
+        type: "password",
+        placeholder: "API Key",
+        icon: "key"
+      },
+      {
+        name: "SecretAPIKey",
+        title: "Secret API Key",
+        description: "you can get your Secret API key in @CryptoAdGateWayBot",
+        type: "password",
+        placeholder: "Secret API Key",
+        icon: "key"
+      },
+      {
+        name: "WebhookUrl",
+        title: "Notifications url. Fill this notifications url on API key creation",
+        description: webhookUrl,
+        icon: "flash"
+      },
+      {
+        name: "OnNotification",
+        title: "Command to be called on notifications",
+        description: "this command will be executed on notification",
+        type: "string",
+        placeholder: "/onNotify",
+        icon: "notifications"
+      },
+    ]
+  }
+  
+  AdminPanel.setPanel({
+    panel_name: lib.panelName,
+    data: panel,
+    force: false // default false - save fields values
+  });
+}
