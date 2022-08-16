328de69147b18a4b9b8b5031583ec94121e6e623
DevBikini <110376121+DevBikini@users.noreply.github.com>
2022-08-16 11:04:53 +0800

    Update _ONnotification.js

--- commands/_ONnotification.js
+++ commands/_ONnotification.js
@@ -1,1 +1,20 @@
-Bot.inspect(content)
+//inspect Bot.inspect(content)
+if (content.deposit) {
+  return Bot.run({
+    command: "Your command deposit notification",
+    options: { result: content }
+  })
+}
+if (content.address) {
+  return Bot.run({
+    command: "Your command deposit generate address",
+    options: { result: content }
+  })
+}
+if (content.withdraw) {
+  return Bot.run({
+    command: "Your command withdrawals notification",
+    options: { result: content }
+  })
+}
+
