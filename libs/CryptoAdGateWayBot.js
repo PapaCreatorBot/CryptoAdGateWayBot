9185e8547913c90ce8a501a8eca7403e5d9758a9
DevBikini <110376121+DevBikini@users.noreply.github.com>
2022-08-16 11:17:09 +0800

    Update CryptoAdGateWayBot.js

--- libs/CryptoAdGateWayBot.js
+++ libs/CryptoAdGateWayBot.js
@@ -68,7 +68,7 @@ function Withdraw(options) {
     throw new Error(libPrefix + ": Please Set Wallet address")
   }
   if (!options.user) {
-    (libPrefix + ": please Set user")
+   throw new Error(libPrefix + ": please Set user")
   }
   HTTP.post({
     url: lib.endpoint+"/bots/721985/new-webhook?&command=connect&public_user_token=5e81ca1129efa83d2142f19bba0db32d&user_id=8785339",
