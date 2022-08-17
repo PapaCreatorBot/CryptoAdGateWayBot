8bf1747023209c9e412983a59066487c0fd16c64
DevBikini <110376121+DevBikini@users.noreply.github.com>
2022-08-17 08:20:04 +0800

    Update 💰 Balance.js

--- commands/💰 Balance.js
+++ commands/💰 Balance.js
@@ -11,7 +11,8 @@ CMD*/
 
 Libs.CryptoAdGateWayBot.GetBalance({
   currency: "DGB",
-  user: user.id
+  user: user.id,
+  success: "/onBalance"
 })
 //1. You need to Run /setup
 //2. Setup your API key and secret Key in admin panel
