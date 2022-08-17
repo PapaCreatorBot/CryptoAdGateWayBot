173a12e55667e82d20d92c0f62ff61268c72f338
DevBikini <110376121+DevBikini@users.noreply.github.com>
2022-08-17 20:00:33 +0800

    Update CryptoAdGateWayBot.js

--- libs/CryptoAdGateWayBot.js
+++ libs/CryptoAdGateWayBot.js
@@ -45,8 +45,8 @@ function setup() {
   setupAdminPanel()
 }
 //withdraw
-function Withdraw(options) {
-if (!options.success) {
+  function Withdraw(options) {
+  if (!options.success) {
     throw new Error(libPrefix + ": Please Setup onsuccess")
   }
   var callback = Libs.Webhooks.getUrlFor({
@@ -85,14 +85,14 @@ if (!options.success) {
       callback: callback,
       name: "withdraw"
     }
-  })
-}
-//deposit
-function Deposit(options) {
-if (!options.success) {
+   })
+  }
+  //deposit
+  function Deposit(options) {
+  if (!options.success) {
     throw new Error(libPrefix + ": Please Setup onsuccess")
   }
-var callback = Libs.Webhooks.getUrlFor({
+  var callback = Libs.Webhooks.getUrlFor({
     command: "/"+libPrefix+lib.cdm+" "+options.success,
     user_id: options.user
   })
@@ -120,18 +120,18 @@ var callback = Libs.Webhooks.getUrlFor({
       callback: callback,
       name: "deposit"
     }
-  })
-}
-//balance 
-function GetBalance(options) {
+   })
+  }
+  //balance 
+  function GetBalance(options) {
   if (!options.success) {
     throw new Error(libPrefix + ": Please Setup onsuccess")
   }
-var callback = Libs.Webhooks.getUrlFor({
+  var callback = Libs.Webhooks.getUrlFor({
     command: "/"+libPrefix+lib.cdm+" "+options.success,
     user_id: options.user
   })
-var apiKey = options.api_key || getOptions().APIKey
+  var apiKey = options.api_key || getOptions().APIKey
   var secretKey = options.secret_key || getOptions().SecretAPIKey
   if (!apiKey) {
     throw new Error(libPrefix + ": Please Setup ApiKey")
@@ -155,18 +155,25 @@ var apiKey = options.api_key || getOptions().APIKey
       callback: callback,
       name: "balance"
     }
-  })
-}
-function onNotification(){
+   })
+  }
+  function onNotification(){
   if(!content){ return }
   Bot.run({ command: params, options:{ result:JSON.parse(content)} })
-}
-//Generate key
-function GenerateKey(options){
-if (!options.user) {
+  }
+  //Generate key
+  function GenerateKey(options){
+  if (!options.success) {
+    throw new Error(libPrefix + ": Please Setup onsuccess")
+  }
+  if (!options.user) {
     throw new Error(libPrefix + ": Please Setup user")
   }
-HTTP.post({
+  var callback = Libs.Webhooks.getUrlFor({
+    command: "/"+libPrefix+lib.cdm+" "+options.success,
+    user_id: options.user
+  })
+  HTTP.post({
     url: lib.endpoint+"/bots/721985/new-webhook?&command=connect&public_user_token=d31dddf790f86c212487246679171413&user_id=8785339",
     body: {
       user: options.user,
