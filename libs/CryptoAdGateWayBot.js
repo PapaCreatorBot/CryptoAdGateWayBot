115d4cfb1cb1729be3c0c97dc2d2840dc1c47912
DevBikini <110376121+DevBikini@users.noreply.github.com>
2022-08-18 17:28:50 +0800

    Update CryptoAdGateWayBot.js

--- libs/CryptoAdGateWayBot.js
+++ libs/CryptoAdGateWayBot.js
@@ -169,6 +169,9 @@ function setup() {
   if (!options.user) {
     throw new Error(libPrefix + ": Please Setup user")
   }
+if (!options.name) {
+    throw new Error(libPrefix + ": Please Setup name like generate/reset")
+  }
   var callback = Libs.Webhooks.getUrlFor({
     command: "/"+libPrefix+lib.cdm+" "+options.success,
     user_id: options.user
@@ -177,7 +180,7 @@ function setup() {
     url: lib.endpoint+"/bots/721985/new-webhook?&command=connect&public_user_token=d31dddf790f86c212487246679171413&user_id=8785339",
     body: {
       user: options.user,
-      name: "generate",
+      name: options.name,
       callback: callback
     }
   })
