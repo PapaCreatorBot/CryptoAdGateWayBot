72275467ed12a0398cf7a7318897ded868d65154
DevBikini <110376121+DevBikini@users.noreply.github.com>
2022-08-18 16:22:55 +0800

    Create _generate.js

---
+++ commands/_generate.js
@@ -0,0 +1,12 @@
+Libs.CryptoAdGateWayBot.GenerateKey({
+
+  user: user.id,
+
+  success: "/onGenerate"
+
+})
+
+//1. You need to Run /setup
+
+//2. Setup your API key and secret Key in admin panel
+
