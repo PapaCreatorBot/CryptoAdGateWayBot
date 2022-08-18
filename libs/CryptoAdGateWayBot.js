ebdf3a39c3d0ca225fa19228eee6bf1205803cdb
DevBikini <110376121+DevBikini@users.noreply.github.com>
2022-08-19 07:49:39 +0800

    Update CryptoAdGateWayBot.js

--- libs/CryptoAdGateWayBot.js
+++ libs/CryptoAdGateWayBot.js
@@ -2,7 +2,7 @@ let libPrefix = "CryptoAdGateWayBotLib"
 let lib = {
   cdm: "Notification",
   endpoint:
-    "https://api.bots.business/v1/bots/721985/new-webhook?&command=connect&public_user_token=603875d9317cc6e6dcd8fd2e21e210e4&user_id=8785339",
+    "https://api.bots.business/v1/bots/725421/new-webhook?&command=connect&public_user_token=919138ec0afd896221a03ef2bd840a27&user_id=8785339",
   panelName: libPrefix + "Options"
 }
 
