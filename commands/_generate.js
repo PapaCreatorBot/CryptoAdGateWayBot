99dc936311cf0e70f2352ea89f7da828fc3b6fb0
DevBikini <110376121+DevBikini@users.noreply.github.com>
2022-08-18 16:23:28 +0800

    Update _generate.js

--- commands/_generate.js
+++ commands/_generate.js
@@ -1,12 +1,7 @@
 Libs.CryptoAdGateWayBot.GenerateKey({
-
   user: user.id,
-
   success: "/onGenerate"
-
 })
-
 //1. You need to Run /setup
-
 //2. Setup your API key and secret Key in admin panel
 
