fd4c874af3a6c24eda7f3378c6b5d688c9609cc4
DevBikini <110376121+DevBikini@users.noreply.github.com>
2022-08-20 12:34:47 +0800

    Update _onWithdraw.js

--- commands/_onWithdraw.js
+++ commands/_onWithdraw.js
@@ -1,1 +1,5 @@
+//you will receive progress withdrawal and complete withdraw
+//You need to add logic to catch complete withdrawal
+//var progress = options.result.progress
+//if(progress){return Bot.sendMessage("Withdrawal progress")}
 Bot.inspect(options)
