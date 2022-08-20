3001ea9a06b9111b5c500416e14b901b2fb0c93f
DevBikini <110376121+DevBikini@users.noreply.github.com>
2022-08-20 12:32:15 +0800

    Update _onDeposit.js

--- commands/_onDeposit.js
+++ commands/_onDeposit.js
@@ -1,1 +1,5 @@
+//you will receive deposit address and complete deposit
+//You can use logic to catch complete deposit
+//var address = options.result.address
+//if(address){return Bot.sendMessage("`"+address+"`")}
 Bot.inspect(options)
