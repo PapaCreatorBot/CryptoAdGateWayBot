08cf867a1e698c51773b822d6b525709eff6c6ae
Kris <codermanager80@gmail.com>
2023-03-08 05:51:08 +0800

    Update _onWithdraw.js

--- commands/_onWithdraw.js
+++ commands/_onWithdraw.js
@@ -9,8 +9,8 @@
   aliases: 
 CMD*/
 
-//you will receive progress withdrawal and complete withdraw
-//You need to add logic to catch complete withdrawal
-//var progress = options.result.progress
-//if(progress){return Bot.sendMessage("Withdrawal progress")}
+var error = options.result
+if(error.error){
+return Bot.sendMessage(error.message)
+}
 Bot.inspect(options)
