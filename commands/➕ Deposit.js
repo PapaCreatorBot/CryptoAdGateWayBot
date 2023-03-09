c364ed580e724c58f5b0d5dc2c27317c3eb7c2e9
Kris <codermanager80@gmail.com>
2023-03-09 21:06:38 +0800

    Update ➕ Deposit.js

--- commands/➕ Deposit.js
+++ commands/➕ Deposit.js
@@ -9,7 +9,11 @@
   aliases: 
 CMD*/
 
+var api_key = "YOUR API KEY HERE"
+var secret_key = "SECRET KEY HERE"
 Libs.CryptoAdGateWayBot.Deposit({
+  api_key:api_key,
+  secret_key: secret_key,
   currency: "DGB",
   user: user.id,
   success: "/onDeposit"
