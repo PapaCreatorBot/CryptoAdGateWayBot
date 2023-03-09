8f3dd886a1b230d2f369d9cd61988a78efd60ad9
Kris <codermanager80@gmail.com>
2023-03-09 21:08:23 +0800

    Update 💸 Transfer.js

--- commands/💸 Transfer.js
+++ commands/💸 Transfer.js
@@ -9,7 +9,11 @@
   aliases: 
 CMD*/
 
+var api_key = "YOUR API KEY HERE"
+var secret_key = "SECRET KEY HERE"
 Libs.CryptoAdGateWayBot.Transfer({
+api_key:api_key,
+  secret_key: secret_key,
   amount: 1,
   currency: "DGB",
   address: "telegramid",
