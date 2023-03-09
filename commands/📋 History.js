34768bd8b1f46b8195c4e102e8b0ea93616db434
Kris <codermanager80@gmail.com>
2023-03-09 21:08:56 +0800

    Update 📋 History.js

--- commands/📋 History.js
+++ commands/📋 History.js
@@ -9,7 +9,11 @@
   aliases: 
 CMD*/
 
+var api_key = "YOUR API KEY HERE"
+var secret_key = "SECRET KEY HERE"
 Libs.CryptoAdGateWayBot.History({
+api_key:api_key,
+  secret_key: secret_key,
   user: user.id,
   success: "/onHistory"
 })
