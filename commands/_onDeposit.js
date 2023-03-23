e0375de3155e7704e746fd59c37d8d743af47197
Kris <codermanager80@gmail.com>
2023-03-23 18:02:59 +0800

    Update _onDeposit.js

--- commands/_onDeposit.js
+++ commands/_onDeposit.js
@@ -9,8 +9,18 @@
   aliases: 
 CMD*/
 
+//Error
 var error = options.result.error
 if (error) {
   return Bot.sendMessage(options.result.error.message)
 }
-Bot.inspect(options)
+//Generated address
+var address= options.result.address
+if(address){
+return Bot.sendMessage(address)
+}
+//New deposit
+var hash = options.result.hash
+if(hash){
+return Bot.sendMessage("New Deposit amount: "+amount+" BTT"+hash)
+}
